1cb1cc98d7e43372c729299f1f87b276b7dcb0d1
Rufaidashafiq <rufaidashafik@gmail.com>
2021-03-30 17:08:33 +0500

    buffer and stream

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
 const fs= require('fs')
-// read file
-// fs.readFile('node.txt' , (err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         // console.log(data.toString())
-//         console.log(data)
-//     }
-// })
-// write file
-// fs.writeFile('node.txt' ,'A new message' ,(err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Message created')
-//     }
-// })
-// append file
-// fs.appendFile('node.txt' ,'A new message 2\r\n' ,(err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Message created')
-//     }
-// })
-if(!fs.existsSync('new folder')){
-fs.mkdir('new folder' ,(err, data) =>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('folder created')
-    }
+const writeStream= fs.createWriteStream('write.txt')
+const readStream = fs.createReadStream('node.txt', {
+    encoding: 'utf-8'
 })
-}else{
-  console.log("folder already exist")
-}
-
+readStream.on('data', chunk =>{
+    console.log(chunk)
+    writeStream.write(chunk)
+})
 

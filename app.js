b4b8043f56b68763535f1c0315fa309350adf03e
Rufaidashafiq <rufaidashafik@gmail.com>
2021-03-30 16:45:59 +0500

    Covered file system

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
-const api = require('./users')
-const {nums, results} = require('./users')
-console.log(api)
-console.log(api.nums)
+const fs= require('fs')
+// read file
+// fs.readFile('node.txt' , (err, data) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         // console.log(data.toString())
+//         console.log(data)
+//     }
+// })
+// write file
+// fs.writeFile('node.txt' ,'A new message' ,(err, data) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('Message created')
+//     }
+// })
+// append file
+// fs.appendFile('node.txt' ,'A new message 2\r\n' ,(err, data) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('Message created')
+//     }
+// })
+if(!fs.existsSync('new folder')){
+fs.mkdir('new folder' ,(err, data) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('folder created')
+    }
+})
+}else{
+  console.log("folder already exist")
+}
+
+

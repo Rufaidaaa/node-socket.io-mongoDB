2a9622b609fed9d72fe06b2fcfa442965d7627fc
Rufaidashafiq <rufaidashafik@gmail.com>
2021-04-12 22:31:00 +0500

    node routing without express

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const http = require("http");
 const fs = require("fs");
 const port = 3000;
+
 const hostname = "localhost";
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-type", "text/html");
-  fs.readFile("./view/index.html", (err, data) => {
+
+  let route = "./view/";
+
+  switch (req.url) {
+    case "/":
+      route += "index.html";
+      break;
+    case "/contact":
+      route += "contact.html";
+      break;
+
+    default:
+      route += "404.html";
+      break;
+  }
+  fs.readFile(route, (err, data) => {
     if (err) {
       console.log(err);
       res.end();

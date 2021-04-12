fad33848969c556eac4ce57a190accf0b95610c0
Rufaidashafiq <rufaidashafik@gmail.com>
2021-04-12 21:09:26 +0500

    return html in node

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const port = 3000;
 const hostname = "localhost";
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-type", "text/html");
-  res.write("<h1>hello</h1>");
-  res.end();
+  fs.readFile("./view/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(port, hostname, () => {

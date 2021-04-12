b1b742ebe8126766bb3521e8bb224c108c8a3458
Rufaidashafiq <rufaidashafik@gmail.com>
2021-04-12 20:45:27 +0500

    create a server

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-const fs= require('fs')
-// read file
-// fs.readFile('node.txt' , (err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         // console.log(data.toString())
-//         console.log(data)
-//     }
-// })
-// write file
-// fs.writeFile('node.txt' ,'A new message' ,(err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Message created')
-//     }
-// })
-// append file
-// fs.appendFile('node.txt' ,'A new message 2\r\n' ,(err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Message created')
-//     }
-// })
-if(!fs.existsSync('new folder')){
-fs.mkdir('new folder' ,(err, data) =>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('folder created')
-    }
-})
-}else{
-  console.log("folder already exist")
-}
+const http = require("http");
+const port = 3000;
+const hostname = "localhost";
 
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
 
+server.listen(port, hostname, () => {
+  console.log(`listening on port ${port}`);
+});
